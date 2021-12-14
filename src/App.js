ba0e27cb588c2044453fc346bbaac28c8bfa15e7
unknown <tariq.1994t12@gmail.com>
2021-12-14 19:16:59 +0200

    Fc

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+import Header from './components/Layouts/Header';
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header />
+      </Fragment>
+      
+      
+    </div>
+  );
+}
+
+export default App;
