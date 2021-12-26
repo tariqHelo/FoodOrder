9cac2e44f144696eb77a044d153168d10999ea7e
unknown <tariq.1994t12@gmail.com>
2021-12-27 01:10:29 +0200

    AddModalView

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,23 +1,21 @@
-import { Fragment } from "react";
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
-import HeaderCartButton from './HeaderCartButton'
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
-
-return <Fragment>
-    
-    <header className={classes.header}>
-        <h1>Food Meals</h1>
-        <HeaderCartButton />
-    </header>
-    <div className={classes['main-image']}>
-    
-        <img src={mealsImage} alt='Meals Image full delicuss food' />
-    </div>
-
-</Fragment>
-
-} 
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
