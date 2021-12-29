99d77937958ef73e0c2006f96ba00fc6a6567c7b
unknown <tariq.1994t12@gmail.com>
2021-12-30 00:14:32 +0200

    LastUpdateContextApi

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,20 +1,35 @@
-import classes from './MealItem.module.css'
+import { useContext } from 'react';
+
 import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
+
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
 
-    const price = `$${props.price.toFixed(2)}`;
+  const price = `$${props.price.toFixed(2)}`;
 
-    return <li className={classes.meal}>
-        <div>
-            <h3>{props.name}</h3>
-            <div>{props.description}</div>
-            <div className={classes.price}>{price}</div>
-        </div>
-        <div>
-         <MealItemForm />
-        </div>
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
     </li>
+  );
 };
 
-export default MealItem
+export default MealItem;
