7a841296965ac702a4336d3f04c5a41b6e59d991
unknown <tariq.1994t12@gmail.com>
2022-02-25 23:02:52 +0200

    AddFormCheckoutUser

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,16 @@
-import { useContext } from 'react';
+import { useContext , useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -23,6 +28,19 @@ const Cart = (props) => {
     cartCtx.addItem({...item, amount: 1});
   };
 
+  const orderHandler = () =>{
+    setIsCheckout(true)
+  }
+  const submitOrderHandler = (userData) =>{
+    fetch('https://foodapp-95955-default-rtdb.asia-southeast1.firebasedatabase.app/meals/orders.json',{
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderdItems:cartCtx.items,
+      })
+    })
+  }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -37,6 +55,14 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+  
+  const modalActions = 
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+      </div>
 
   return (
     <Modal onClose={props.onClose}>
@@ -45,12 +71,10 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+        )}
+      {!isCheckout && modalActions}
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,50 +1,99 @@
+import { useEffect , useState } from 'react';
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
+   const [meals, setmeals] = useState([]);
+   const [isLeading, setIsLeading] = useState(true);
+   const [httpError, sethttpError] = useState();
+   useEffect(() => {
+     const fetchMeals = async () => {
+       const response = await fetch('https://foodapp-95955-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+
+       if(!response.ok){
+         throw new Error("Something went Error !");
+
+       }
+
+       const responsedData = await response.json();
+       const loadedMeals = [];
+       for (const key in responsedData) {
+         loadedMeals.push({
+           id:key,
+           name:responsedData[key].name,
+           description:responsedData[key].description,
+           price:responsedData[key].price,
+         });
+       }
+       setmeals(loadedMeals);
+       setIsLeading(false)
+     };
+
+    
+    fetchMeals().catch((error) => {
+      setIsLeading(false);
+      sethttpError(error.message)
+    });
+
+   }, [])
+
   
-    const mealslist = DUMMY_MEALS.map((meal) => 
+  if (isLeading){
+    return <section className={classes.MealsIsLoading}>
+      Loading.....
+    </section>
+  } 
+
+  if(httpError){
+    return <section className={classes.MealsError}>
+      {httpError}
+    </section>
+  }
+
+
+  const mealslist = meals.map((meal) => 
     <MealItem 
       id={meal.id}
       key={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price} 
-    />)
+    />
+    )
 
-    return <section className={classes.meals}>
-      <Card>
-        <ui>{mealslist}</ui>
-      </Card>
-    </section>
+  return <section className={classes.meals}>
+    <Card>
+      <ui>{mealslist}</ui>
+    </Card>
+  </section>
 
 };
 
